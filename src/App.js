d08666ea298768f5d510329f8cf116184216690e
Nataly Makarova <natmaklis@gmail.com>
2020-04-24 17:43:56 +0300

    delete all bind(this), end used array fuction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import "./App.css";
 import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
+import Search from "./components/search/Search.jsx";
+import Table from "./components/table/Table.jsx";
+import Button from "./components/button/Button.jsx";
 
 const DEFAULT_QUERY = "redux";
 const DEFAULT_HPP = "100";
@@ -26,19 +29,18 @@ class App extends Component {
       error: null,
     };
 
-    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
+    // this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+    // this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    // this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    // this.onSearchChange = this.onSearchChange.bind(this);
+    // this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    // this.onDismiss = this.onDismiss.bind(this);
+    //
   }
 
-  needsToSearchTopStories(searchTerm) {
-    return !this.state.results[searchTerm];
-  }
+  needsToSearchTopStories = (searchTerm) => !this.state.results[searchTerm];
 
-  setSearchTopStories(result) {
+  setSearchTopStories = (result) => {
     const { hits, page } = result;
     const { searchKey, results } = this.state;
 
@@ -53,15 +55,15 @@ class App extends Component {
         [searchKey]: { hits: updatedHits, page },
       },
     });
-  }
+  };
 
-  fetchSearchTopStories(searchTerm, page = 0) {
+  fetchSearchTopStories = (searchTerm, page = 0) => {
     axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
       .then((result) => this.setSearchTopStories(result.data))
       .catch((error) => this._isMounted && this.setState({ error }));
-  }
+  };
 
   componentDidMount() {
     this._isMounted = true;
@@ -75,11 +77,9 @@ class App extends Component {
     this._isMounted = false;
   }
 
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
-  }
+  onSearchChange = (event) => this.setState({ searchTerm: event.target.value });
 
-  onSearchSubmit(event) {
+  onSearchSubmit = (event) => {
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
 
@@ -88,9 +88,9 @@ class App extends Component {
     }
 
     event.preventDefault();
-  }
+  };
 
-  onDismiss(id) {
+  onDismiss = (id) => {
     const { searchKey, results } = this.state;
     const { hits, page } = results[searchKey];
 
@@ -103,7 +103,7 @@ class App extends Component {
         [searchKey]: { hits: updatedHits, page },
       },
     });
-  }
+  };
 
   render() {
     const { searchTerm, results, searchKey, error } = this.state;
@@ -143,51 +143,5 @@ class App extends Component {
     );
   }
 }
-
-const Search = ({ value, onChange, onSubmit, children }) => (
-  <form onSubmit={onSubmit}>
-    <input type="text" value={value} onChange={onChange} />
-    <button type="submit">{children}</button>
-  </form>
-);
-
-const Table = ({ list, onDismiss }) => (
-  <div className="table">
-    {list.map((item) => (
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: "40%" }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={{ width: "30%" }}>{item.author}</span>
-        <span style={{ width: "10%" }}>{item.num_comments}</span>
-        <span style={{ width: "10%" }}>{item.points}</span>
-        <span style={{ width: "10%" }}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    ))}
-  </div>
-);
-
-const Button = ({ onClick, className = "", children }) => (
-  <button onClick={onClick} className={className} type="button">
-    {children}
-  </button>
-);
-// PropTypes.array
-// • PropTypes.bool
-// • PropTypes.func
-// • PropTypes.number
-// • PropTypes.object
-// • PropTypes.string
-// Однако, у вас есть ещё два PropTypes для определения отрисованного фрагмента (узла),
-// например, строка и элемент React:
-// • PropTypes.node
-// • PropTypes.element
 
 export default App;
