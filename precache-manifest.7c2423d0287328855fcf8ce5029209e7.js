35e970488f71c7aba6417a1ba04a32edcaa6d98b
Nataly Makarova <natmaklis@gmail.com>
2020-05-01 12:33:24 +0300

    Updates

---
+++ precache-manifest.7c2423d0287328855fcf8ce5029209e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d3a6e03e3cac7ad63c561a201d6a14",
+    "url": "/NewsAPI__react/index.html"
+  },
+  {
+    "revision": "268880a0024a598fc296",
+    "url": "/NewsAPI__react/static/css/main.3b6c023d.chunk.css"
+  },
+  {
+    "revision": "7e5ce294dca2d852163f",
+    "url": "/NewsAPI__react/static/js/2.0226c50b.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/NewsAPI__react/static/js/2.0226c50b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268880a0024a598fc296",
+    "url": "/NewsAPI__react/static/js/main.fc19e1f6.chunk.js"
+  },
+  {
+    "revision": "fcbe2b5e1f8fe257f73f",
+    "url": "/NewsAPI__react/static/js/runtime-main.e96130bc.js"
+  }
+]);
